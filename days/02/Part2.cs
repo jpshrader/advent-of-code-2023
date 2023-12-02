c7b1746dd01bd09a890a8eecb590d1414bb18271
Jack <96661942+jpshrader@users.noreply.github.com>
2023-12-02 11:09:04 -0500

    refactor the 1 am brain

--- days/02/Part2.cs
+++
@@ -1,7 +0,0 @@
-namespace day_2 {
-    public static class Part2 {
-        public static int Run(IEnumerable<string> lines) {
-            return -1;
-        }
-    }
-}
--- days/02/Program.cs
+++ days/02/Program.cs
@@ -1,17 +1,62 @@
-﻿using day_2;
-
-var lines = File.ReadLines("input.txt");
+﻿var lines = File.ReadLines("input.txt");
 if (lines == null || !lines.Any()) {
     Console.WriteLine("file/lines not found");
     return -1;
 }
 lines = lines.Select(l => l.Trim().ToLower());
 
-var part1 = Part1.Run(lines);
-var part2 = Part2.Run(lines);
+var games = new List<Game>();
+foreach (var line in lines) {
+    games.Add(new Game(line));
+}
+
+var part1 = Part1(games);
 
 Console.WriteLine($"{new string('=', 10)} SOLUTIONS {new string('=', 10)}");
 Console.WriteLine($"Part 1: {part1}");
-Console.WriteLine($"Part 2: {part2}");
 
 return 0;
+
+static int Part1(List<Game> games) {
+    return games
+        .Where(g => g.IsPossible(redLimit: 12, greenLimit: 13, blueLimit: 14))
+        .Sum(g => g.GameId);
+}
+
+class Game {
+    public int GameId { get; set; }
+    public IEnumerable<Set> Sets { get; set; }
+    public Game(string line) {
+        var parts = line.Split(':');
+
+        GameId = int.Parse(parts[0].Replace("game", "").Trim());
+        Sets = parts[1].Split(';').Select(s => new Set(s));
+    }
+
+    public bool IsPossible(int redLimit, int greenLimit, int blueLimit) {
+        return Sets.All(s => s.IsPossible(redLimit, greenLimit, blueLimit));
+    }
+}
+
+class Set {
+    public int Red { get; set; }
+    public int Green { get; set; }
+    public int Blue { get; set; }
+
+    public Set(string line) {
+        var parts = line.Split(',');
+        foreach (var part in parts) {
+            if (part.Contains("red")) {
+                Red = int.Parse(part.Replace("red", "").Trim());
+            } else if (part.Contains("green")) {
+                Green = int.Parse(part.Replace("green", "").Trim());
+            } else if (part.Contains("blue")) {
+                Blue = int.Parse(part.Replace("blue", "").Trim());
+            }
+        }
+    }
+
+    public bool IsPossible(int redLimit, int greenLimit, int blueLimit) {
+        return Red <= redLimit && Green <= greenLimit && Blue <= blueLimit;
+    }
+}
