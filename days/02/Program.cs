8a2f9119a0cc22e9615609c15dce986fcf935ecb
Jack <96661942+jpshrader@users.noreply.github.com>
2023-12-02 00:55:13 -0500

    day 2, part 1

---
+++ days/02/Program.cs
@@ -0,0 +1,13 @@
+﻿using day_2;
+
+var lines = File.ReadLines("input.txt");
+if (lines == null || !lines.Any()) {
+    Console.WriteLine("file/lines not found");
+    return -1;
+}
+lines = lines.Select(l => l.Trim().ToLower());
+
+var part1 = Part1.Run(lines);
+Console.WriteLine($"Part 1: {part1}");
+
+return 0;
