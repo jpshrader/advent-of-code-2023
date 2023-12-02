fbce580e17782e4818b0430ada097b67223f8e3c
Jack <96661942+jpshrader@users.noreply.github.com>
2023-12-02 00:58:02 -0500

    deux

--- days/02/Program.cs
+++ days/02/Program.cs
@@ -8,6 +8,10 @@ if (lines == null || !lines.Any()) {
 lines = lines.Select(l => l.Trim().ToLower());
 
 var part1 = Part1.Run(lines);
+var part2 = Part2.Run(lines);
+
+Console.WriteLine($"{new string('=', 10)} SOLUTIONS {new string('=', 10)}");
 Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
 
 return 0;
