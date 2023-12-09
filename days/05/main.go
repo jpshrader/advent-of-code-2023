950519274ced9f9e47230b5a13d07d9755d8ffb4
Jack <96661942+jpshrader@users.noreply.github.com>
2023-12-09 11:18:00 -0500

    read file

--- days/05/main.go
+++ days/05/main.go
@@ -5,31 +5,100 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	_, err := readFile()
+	lookup, seeds, err := readFile()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
+	for key, value := range lookup {
+		fmt.Println(key, len(value))
+	}
 
 	fmt.Println("========== SOLUTIONS ==========")
-	fmt.Println("Part 1:", -1)
+	fmt.Println("Part 1: ", len(seeds))
 }
 
-func readFile() ([]string, error) {
+const (
+	seedLineId string = "seeds: "
+
+	seedToSoilMapLineId            string = "seed-to-soil map:"
+	soilToFertilizerMapLineId      string = "soil-to-fertilizer map:"
+	fertilizerToWaterMapLineId     string = "fertilizer-to-water map:"
+	waterToLightMapLineId          string = "water-to-light map:"
+	lightToTemperatureMapLineId    string = "light-to-temperature map:"
+	temperatureToHumidityMapLineId string = "temperature-to-humidity map:"
+	humidityToLocationMapLineId    string = "humidity-to-location map:"
+)
+
+type mapItem struct {
+	Beginning int
+	Middle    int
+	End       int
+}
+
+type mapLookup map[string][]mapItem
+
+func readFile() (mapLookup, []int, error) {
 	file, err := os.Open("input.txt")
 	if err != nil {
-		return []string{}, err
+		return mapLookup{}, []int{}, err
 	}
 	defer file.Close()
 
-	lines := []string{}
+	lookup := mapLookup{
+		seedToSoilMapLineId:            []mapItem{},
+		soilToFertilizerMapLineId:      []mapItem{},
+		fertilizerToWaterMapLineId:     []mapItem{},
+		waterToLightMapLineId:          []mapItem{},
+		lightToTemperatureMapLineId:    []mapItem{},
+		temperatureToHumidityMapLineId: []mapItem{},
+		humidityToLocationMapLineId:    []mapItem{},
+	}
+	var target string
+	seeds := []int{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		line := scanner.Text()
+
+		if strings.Contains(line, seedLineId) {
+			seedIds := strings.Split(strings.Replace(line, seedLineId, "", 1), " ")
+			for _, seedId := range seedIds {
+				parsedSeedId, _ := strconv.Atoi(seedId)
+				seeds = append(seeds, parsedSeedId)
+			}
+			continue
+		}
+
+		target = processLine(lookup, target, line)
 	}
-	return lines, scanner.Err()
+	return lookup, seeds, scanner.Err()
+}
+
+func processLine(lookup mapLookup, currentTargetId string, line string) string {
+	if len(line) == 0 {
+		return ""
+	}
+
+	for lineId := range lookup {
+		if strings.Contains(line, lineId) {
+			return lineId
+		}
+	}
+
+	splitLine := strings.Split(line, " ")
+	beginning, _ := strconv.Atoi(splitLine[0])
+	middle, _ := strconv.Atoi(splitLine[1])
+	end, _ := strconv.Atoi(splitLine[2])
+	lookup[currentTargetId] = append(lookup[currentTargetId], mapItem{
+		Beginning: beginning,
+		Middle:    middle,
+		End:       end,
+	})
+
+	return currentTargetId
 }
